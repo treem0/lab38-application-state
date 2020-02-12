2face537a71aba3d918f8e25a310920fe85d6a7f
Travis Molter <travismolter@gmail.com>
2020-02-12 15:14:42 -0800

    snack action test working in my reducer

--- src/reducers/emojiReducer.js
+++ src/reducers/emojiReducer.js
@@ -10,11 +10,11 @@ export default function reducer(state, action) {
     case DRINK_COFFEE:
       return { ...state, coffees: state.coffees + 1 };
     case EAT_SNACK:
-      return { ...state, coffees: state.coffees + 1 };
+      return { ...state, snacks: state.snacks + 1 };
     case TAKE_NAP:
-      return { ...state, coffees: state.coffees + 1 };
+      return { ...state, naps: state.naps + 1 };
     case STUDY:
-      return { ...state, coffees: state.coffees + 1 };
+      return { ...state, studies: state.studies + 1 };
     default:
       return state;
   }
--- src/reducers/emojiReducer.test.js
+++ src/reducers/emojiReducer.test.js
@@ -1,4 +1,5 @@
 import reducer from './emojiReducer';
+import { drinkCoffee, eatSnack } from '../actions/emojiActions';
 
 
 describe('emoji reducer', () => {
@@ -8,5 +9,23 @@ describe('emoji reducer', () => {
     const newState = reducer([], action);
 
     expect(newState).toEqual([]);
+  });
+
+  it('handles the drink coffee action', () => {
+    const action = drinkCoffee();
+    const initialState = { coffees: 2 };
+
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({ coffees: 3 });
+  });
+
+  it('handles the eat snack action', () => {
+    const action = eatSnack();
+    const initialState = { snacks: 0 };
+
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({ snacks: 1 });
   });
 });
